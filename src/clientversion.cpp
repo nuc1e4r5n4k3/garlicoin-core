d491dcb5d1b1fa857bca4d9c6e0a8490179fd59b
Erik van `t Wout <erik@ewnetworks.eu>
2022-08-28 23:41:34 +0200

    Version bump/customization

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both bitcoind and bitcoin-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("GarlicoinCore");
+const std::string CLIENT_NAME("GarlicoinFresh");
 
 /**
  * Client version number
